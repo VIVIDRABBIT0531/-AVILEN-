3729d7412c6fb149cc2e97ab1c36f9796eb0abe6
VIVIDRABBIT0531 <68336123+VIVIDRABBIT0531@users.noreply.github.com>
2020-10-15 23:43:41 +0900

    Add files via upload

---
+++ node/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+   const endpoint = req.url;
+   if( endpoint==='/start' ){
+      fs.readFile('./index.html',(err, data)=>{
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.write(data);
+         res.end();
+      });
+   }
+   if( endpoint==='/api' ){
+      const fizzbuzz = it => {
+         if (!(it % 15)) return 'FizzBuzz'
+         if (!(it % 3)) return 'Fizz'
+         if (!(it % 5)) return 'Buzz'
+         return it
+       }
+        
+   }
+});
+server.listen(8080); 
